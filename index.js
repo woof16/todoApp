ce8d04ec0e2adeaf7fa6c3408128989624d612b4
sekijou <alberto.zurita.ro@gmail.com>
2023-08-04 21:01:12 +0200

    Planning.

---
+++ index.js
@@ -0,0 +1,7 @@
+/* 1. Create UI. Divs and style.
+ ** 2. Establish basic functionality.
+ **    Create and delete entries.
+ ** 3. Incorporate crossing out function.
+ ** 4. Add colors.
+ ** 5. Include renaming option.
+ */
